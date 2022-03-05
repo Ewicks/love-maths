72247aed4b77bf177ff87d17fa46de670af47795
Ewicks <ethanowicks9@gmail.com>
2022-03-05 11:24:05 +0000

    set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+
+
+function runGame() {
+
+}
+
+function CheckAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function DisplayAdditionQuestion() {
+
+}
+
+function DisplaySubtractionQuestion() {
+
+}
+
+function displayMultiplyQuestion () {
+    
+}
